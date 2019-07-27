c58c5b77ab415682ce7b26d717f60b47d672199d
duong qaung huy <huydqth1810012@fpt.edu.vn>
2019-07-27 20:09:09 +0700

    BT422 bai1

---
+++ BT422/bai 1/Bai422Phan1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DuongQuangHUY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yrsofService = 3;
+            int salary = 1500;
+            int bonus = 0;
+
+            if (yrsofService < 5)
+            {
+                if (salary < 500)
+                {
+                    bonus = 100;
+                    Console.WriteLine("bonus  :{0} ", bonus);
+                }
+                else
+                {
+                    bonus = 200;
+                    Console.WriteLine("bonus  :{0} ", bonus);
+                }
+
+
+            }
+            else
+            {
+                bonus = 500;
+                Console.WriteLine("bonus :{0} ", bonus);
+
+            }
+        }
+    }
+}
