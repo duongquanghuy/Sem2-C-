732f63e6e35d63520d7cb7cad97c9c95302447aa
Lê Quốc Duy <duylqth1810004@fpt.edu.vn>
2019-08-14 19:41:27 +0700

    upload BT422.b3 va BT423

---
+++ BT422/bt422b3/bt422b3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace bt422b3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int number;
+            int c = 1;
+            float avg = 0;
+
+            while (c <= 3)
+            {
+                Console.WriteLine("Input a number: ");
+                number = int.Parse(Console.ReadLine());
+                sum += number;
+                c++;
+            }
+            avg = sum / 3;
+            Console.WriteLine("Sum is: {0}; Avg is: {1}", sum, avg);
+        }
+    }
+}
