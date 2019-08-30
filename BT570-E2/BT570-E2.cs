05491db8829c2920403e05d83e5da83fdabdff1b
Lê Quốc Duy <duylqth1810004@fpt.edu.vn>
2019-08-30 18:04:56 +0700

    upload BT604 Q1 & Q2, BT570 E1 & E2, BT565

---
+++ BT570-E2/BT570-E2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace BT570_E2
+{
+    public delegate Boolean PrimeNumberFinder(int n);
+
+    class Program
+    {
+        public static event PrimeNumberFinder OnPrimeNumber;
+
+        static void Main(string[] args)
+        {
+            PrimeNumberFinder snt = CheckPrimeNumber;
+
+            OnPrimeNumber = snt;
+
+            Console.WriteLine("Print Number from 1 to 10: ");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (OnPrimeNumber(i))
+                {
+                    Console.WriteLine(i + " is Prime Number!!!");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static Boolean CheckPrimeNumber(int m)
+        {
+            int count = 0;
+
+            if (m < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(m); i++)
+            {
+                if (m % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count != 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ BT604/BT604-Q1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Text;
+
+namespace BT604
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Programmer ltv = new Programmer(1, "Nguyễn Văn A", "Good Communation, Expert in JAVA", Convert.ToDateTime("28/08/1995"));
+            ltv.ShowInfo();
+        }
+    }
+
+    interface IPerson
+    {
+        string Skills { get; set; }
+        DateTime DateOfBirth { get; }
+        int Age { get; }
+    }
+
+    abstract class Employee {
+        int _id;
+        string _name;
+
+        public int ID { get => _id;}
+        public string Name {
+            get => _name;
+            set
+            {
+                if (value.Length < 3)
+                {
+                    Console.WriteLine("Wrong Input!!! Please Try again!!!");
+                }
+                else
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public Employee(int ID)
+        {
+            _id = ID;
+            _name = "No Name";
+        }
+
+        public Employee(int ID, string name)
+        {
+            _id= ID;
+            _name = name;
+        }
+
+        public Employee()
+        {
+
+        }
+
+        public abstract void ShowInfo();
+    }
+
+    class Programmer : Employee, IPerson
+    {
+        string _skills;
+        DateTime _DOB;
+        int _age;
+
+        public String Skills
+        {
+            get
+            {
+                return this._skills;
+            }
+            set
+            {
+                if (value.Equals(""))
+                {
+                    Console.WriteLine("Wrong Input!!! Please Try again!!!");
+                }
+                else
+                {
+                    this._skills = value;
+                }
+            }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get
+            {
+                return this._DOB;
+            }
+            set
+            {
+                this._DOB = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return (new DateTime(1, 1, 1) + (DateTime.Now - DateOfBirth)).Year - 1;
+            }
+        }
+
+        public Programmer(int id, string name)
+        {
+            _skills = String.Empty;
+            _DOB = DateTime.Now;
+        }
+
+        public Programmer(int id, string name, string skills, DateTime dob) : base(id, name)
+        {
+            _skills = skills;
+            _DOB = dob;
+        }
+
+        public override void ShowInfo()
+        {
+            Console.WriteLine("Id: {0} | Name: {1} | Skills: {2} | DOB: {3} | Age: {4}", ID, Name, Skills, DateOfBirth.ToString("dd/MM/yyyy"), Age);
+        }
+    }
+}
